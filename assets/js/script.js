37ec0685c3b12eac10492a82a6eb325b7d89e4f0
Will <willkim0608@gmail.com>
2022-11-15 17:25:07 -0800

    Add event listener. Add API link

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,52 +4,39 @@ var searchesContainerEl = document.querySelector('#previous-searches-container')
 var cityInputEl = document.querySelector('#search-box');
 var currentWeatherContainer = document.querySelector('#current-weather-container');
 var futureWeatherContainer = document.querySelector('#future-forecast-container');
-var myAPI = "8062480d86820bb8c5aa0929de58588d";
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-  
+    findCityName();
+
+};
+
+var findCityName = function (city) {
     var cityName = cityInputEl.value.trim();
-  
     if (cityName) {
-     getCurrentWeather(cityName)
-  
-      currentWeatherContainer.textContent = '';
-      cityInputEl.value = '';
+        //Get current weather function here
+        cityInputEl.value = '';
     } else {
-      alert('Please enter a city name');
+        alert('Please enter a city name');
     }
-  };
 
-  var getCurrentWeather = function (city) {
-    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + myAPI;
-  
+
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=8062480d86820bb8c5aa0929de58588d';
+
     fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function (data) {
-            console.log(data);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to weather server');
-      });
-  };
-
-
-  var displayCurrentWeather = function (repos, searchTerm) {
-    if (repos.length === 0) {
-      repoContainerEl.textContent = 'No repositories found.';
-      return;
-    }
-  
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to weather server');
+        })
+}
 
 
 
 
-
-  userFormEl.addEventListener('submit', formSubmitHandler);
+userFormEl.addEventListener('submit', formSubmitHandler);
